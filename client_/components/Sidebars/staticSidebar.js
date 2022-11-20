6d4fb45d2637036139476833e9222e8045f526db
unknown <hritikb90@gmail.com>
2022-11-20 20:31:32 +0530

    initial client side code commit

---
+++ client_/components/Sidebars/staticSidebar.js
@@ -0,0 +1,38 @@
+{/* Static sidebar for desktop */ }
+import classNames from "../../utils/classNames";
+
+const StaticSidebar = ({ navigation }) => {
+    return (
+        <div className="hidden md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col">
+            {/* Sidebar component, swap this element with another sidebar if you like */}
+            <div className="flex flex-grow flex-col overflow-y-auto bg-indigo-700 pt-5">
+                <div className="flex flex-shrink-0 items-center px-4">
+                    <img
+                        className="h-8 w-auto"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=300"
+                        alt="Your Company"
+                    />
+                </div>
+                <div className="mt-5 flex flex-1 flex-col">
+                    <nav className="flex-1 space-y-1 px-2 pb-4">
+                        {navigation.map((item) => (
+                            <a
+                                key={item.name}
+                                href={item.href}
+                                className={classNames(
+                                    item.current ? 'bg-indigo-800 text-white' : 'text-indigo-100 hover:bg-indigo-600',
+                                    'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
+                                )}
+                            >
+                                <item.icon className="mr-3 h-6 w-6 flex-shrink-0 text-indigo-300" aria-hidden="true" />
+                                {item.name}
+                            </a>
+                        ))}
+                    </nav>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default StaticSidebar;
---
+++ client_/pages/index.js
@@ -0,0 +1,57 @@
+import { Fragment, useState } from 'react'
+import { Dialog, Menu, Transition } from '@headlessui/react'
+import {
+  Bars3BottomLeftIcon,
+  BellIcon,
+  CalendarIcon,
+  ChartBarIcon,
+  FolderIcon,
+  HomeIcon,
+  InboxIcon,
+  UsersIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline'
+
+import StaticSidebar from '../components/Sidebars/staticSidebar'
+
+import Header from '../components/Header'
+
+const navigation = [
+  { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
+  { name: 'Team', href: '#', icon: UsersIcon, current: false },
+  { name: 'Projects', href: '#', icon: FolderIcon, current: false },
+  { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
+  { name: 'Documents', href: '#', icon: InboxIcon, current: false },
+  { name: 'Reports', href: '#', icon: ChartBarIcon, current: false },
+]
+
+
+
+export default function Example() {
+
+
+  return (
+    <>
+      <div>
+        <StaticSidebar navigation={navigation} />
+        <div className="flex flex-1 flex-col md:pl-64">
+          <Header />
+          <main>
+            <div className="py-6">
+              <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
+                <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
+              </div>
+              <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
+                {/* Replace with your content */}
+                <div className="py-4">
+                  <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+                </div>
+                {/* /End replace */}
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
+  )
+}
